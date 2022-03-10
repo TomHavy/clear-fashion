6edc35f302279072f8abc6607f07ee2aaf6abba8
TomHavy <havy.tom24@gmail.com>
2022-03-10 19:34:29 +0100

    update

--- server/connexion.js
+++ server/connexion.js
@@ -1,8 +1,7 @@
-// tuto : https://dev.to/dalalrohit/how-to-connect-to-mongodb-atlas-using-node-js-k9i
 const { MongoClient } = require('mongodb');
 
 const MONGODB_URI = "mongodb+srv://tomhavy24:clear_fashion@Cluster0.g1yua.mongodb.net/clearfashion?retryWrites=true&w=majority";
-const MONGODB_DB_NAME = 'clearfashion';
+const MONGODB_DB_NAME = 'Cluster0';
 
 async function connect() {
     try {
